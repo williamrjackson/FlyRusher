3a5e38fd30c3a939564e0b5b0e9558021387af60
Bill Jackson <billjackson@cakewalk.com>
2018-05-22 23:46:29 -0400

    Ship model; move ship, not tunnel

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform posTarget;
+    public float followSpeed = .5f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void LateUpdate () {
+        transform.position = Vector3.SmoothDamp(transform.position, posTarget.position, ref velocity, followSpeed);
+	}
+}
--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipMovement : MonoBehaviour {
+    public Transform ship;
     [Range (0.01f, .5f)]
     public float snapSpeed = .1f;
     public TouchAxisCtrl touchAxisControl;
     public float xRange = 4f;
     public float yRange = 4f;
+    public float maxSpeed = 5;
+    public float acceleration = .1f;
 
+    private float m_Speed = 0;
     private bool m_bHasBeenTouching;
     private Vector2 m_InitialXY;
     Vector2 m_TargetXY;
@@ -20,21 +24,26 @@ public class ShipMovement : MonoBehaviour {
             if (!m_bHasBeenTouching)
             {
                 m_bHasBeenTouching = true;
-                m_InitialXY = new Vector2(transform.position.x, transform.position.y);
+                m_InitialXY = new Vector2(ship.position.x, ship.position.y);
             }
             m_TargetXY = new Vector3(m_InitialXY.x + touchAxisControl.GetAxis("Horizontal") * xRange,
-                                   m_InitialXY.y + touchAxisControl.GetAxis("Vertical") * yRange);
+                                     m_InitialXY.y + touchAxisControl.GetAxis("Vertical") * yRange);
         }
         else
         {
             m_bHasBeenTouching = false;
         }
-        Vector2 lerpPos = Vector2.Lerp(new Vector2(transform.position.x, transform.position.y), m_TargetXY, snapSpeed);
-        transform.eulerAngles = new Vector3(Remap(transform.position.y - m_TargetXY.y, -yRange, yRange, -45, 45), 
-                                            transform.eulerAngles.y, 
-                                            Remap(transform.position.x - m_TargetXY.x, -xRange, xRange, -60, 60));
-        transform.position = new Vector3(lerpPos.x, lerpPos.y, transform.position.z);
-	}
+        Vector2 lerpPos = Vector2.Lerp(new Vector2(ship.position.x, ship.position.y), m_TargetXY, snapSpeed);
+        ship.eulerAngles = new Vector3(Remap(ship.position.y - m_TargetXY.y, -yRange, yRange, -60, 60), 
+                                            ship.eulerAngles.y, 
+                                            Remap(ship.position.x - m_TargetXY.x, -xRange, xRange, -60, 60));
+        ship.position = new Vector3(lerpPos.x, lerpPos.y, ship.position.z);
+        if (m_Speed < maxSpeed)
+        {
+            m_Speed += acceleration;
+        }
+        transform.position += transform.forward * (m_Speed * Time.deltaTime);
+    }
 
     float Remap(float val, float srcMin, float srcMax, float destMin, float destMax)
     {
