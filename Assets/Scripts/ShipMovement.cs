54580b4d662ed3c6645649d2fd5cda910c9f3c80
Bill Jackson <billjackson@cakewalk.com>
2018-05-25 05:03:09 -0400

    On Rails; Curvy tunnel

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -9,10 +9,7 @@ public class ShipMovement : MonoBehaviour {
     public TouchAxisCtrl touchAxisControl;
     public float xRange = 4f;
     public float yRange = 4f;
-    public float maxSpeed = 5;
-    public float acceleration = .1f;
 
-    private float m_Speed = 0;
     private bool m_bHasBeenTouching;
     private Vector2 m_InitialXY;
     Vector2 m_TargetXY;
@@ -24,7 +21,7 @@ public class ShipMovement : MonoBehaviour {
             if (!m_bHasBeenTouching)
             {
                 m_bHasBeenTouching = true;
-                m_InitialXY = new Vector2(ship.position.x, ship.position.y);
+                m_InitialXY = new Vector2(ship.localPosition.x, ship.localPosition.y);
             }
             m_TargetXY = new Vector3(Mathf.Clamp(m_InitialXY.x + touchAxisControl.GetAxis("Horizontal") * xRange, -xRange, xRange),
                                      Mathf.Clamp(m_InitialXY.y + touchAxisControl.GetAxis("Vertical") * yRange, -yRange, yRange));
@@ -33,16 +30,11 @@ public class ShipMovement : MonoBehaviour {
         {
             m_bHasBeenTouching = false;
         }
-        Vector2 lerpPos = Vector2.Lerp(new Vector2(ship.position.x, ship.position.y), m_TargetXY, snapSpeed);
-        transform.eulerAngles = new Vector3(Remap(ship.position.y - m_TargetXY.y, -yRange, yRange, -60, 60), 
-                                            ship.eulerAngles.y, 
-                                            Remap(ship.position.x - m_TargetXY.x, -xRange, xRange, -60, 60));
-        ship.position = new Vector3(lerpPos.x, lerpPos.y, ship.position.z);
-        if (m_Speed < maxSpeed)
-        {
-            m_Speed += acceleration;
-        }
-        transform.position += transform.forward * (m_Speed * Time.deltaTime);
+        Vector2 lerpPos = Vector2.Lerp(new Vector2(ship.localPosition.x, ship.localPosition.y), m_TargetXY, snapSpeed);
+        ship.localEulerAngles = new Vector3(Remap(ship.localPosition.y - m_TargetXY.y, -yRange, yRange, -30, 30), 
+                                            ship.localEulerAngles.y, 
+                                            Remap(ship.localPosition.x - m_TargetXY.x, -xRange, xRange, -90, 90));
+        ship.localPosition = new Vector3(lerpPos.x, lerpPos.y, ship.localPosition.z);
     }
 
     float Remap(float val, float srcMin, float srcMax, float destMin, float destMax)
