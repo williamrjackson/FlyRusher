5a53e44440b8c688706a3dee0dc390127db6cee3
Bill Jackson <billjackson@cakewalk.com>
2018-06-14 02:58:28 -0400

    Obstacle system
    Required model adjustment scene rebuild
    New gif

---
+++ Assets/ObstacleDropper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDropper : MonoBehaviour {
+
+	public GameObject[] obstaclesPrefabs;
+	public float dropInterval = 5f;
+	public float lifetime = 10f;
+
+	private Transform m_ObstacleContainer;
+	private List<GameObject> m_Obsacles;
+	private float m_ElapsedTime = 0;
+	// Use this for initialization
+	void Start () {
+		m_Obsacles = new List<GameObject>();
+		m_ObstacleContainer = new GameObject("ObstacleContainer").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_ElapsedTime += Time.deltaTime;
+		if (m_ElapsedTime >= dropInterval)
+		{
+			InstantiateObstacle();
+			m_ElapsedTime = 0;
+		}
+	}
+	void InstantiateObstacle()
+	{
+		int randomObstacleIndex = Random.Range(0, obstaclesPrefabs.Length -1);
+		GameObject newObstacle = Instantiate(obstaclesPrefabs[randomObstacleIndex], transform);
+		newObstacle.transform.parent = m_ObstacleContainer;
+		m_Obsacles.Add(newObstacle);
+		StartCoroutine(KillCountdown(newObstacle));
+	}
+
+	IEnumerator KillCountdown(GameObject goner)
+	{
+		yield return new WaitForSeconds(lifetime);
+		Destroy(goner);
+	}
+}
--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -2,77 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour {
-    public Properties testProperties;
-    private IEnumerator currentCoroutine;
-    // Use this for initialization
+public class CameraShake : MonoBehaviour {const float maxAngle = 10f;
+    public Properties shakeProperties;
+	IEnumerator currentShakeCoroutine;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            StartShake();
-        }
-    }
-    public void StartShake()
-    {
-        if (currentCoroutine != null)
-        {
-            StopCoroutine(currentCoroutine);
-        }
-        currentCoroutine = Shake(testProperties);
-        StartCoroutine(currentCoroutine);
-    }
+	public void StartShake() {
+		if (currentShakeCoroutine != null) {
+			StopCoroutine (currentShakeCoroutine);
+		}
 
-    float DampingCurve(float x, float dampingPercent)
-    {
-        x = Mathf.Clamp01(x);
-        float a = Mathf.Lerp(2, .25f, dampingPercent);
-        float b = 1 - Mathf.Pow(x, a);
-        return b * b * b;
-    }
+		currentShakeCoroutine = Shake (shakeProperties);
+		StartCoroutine (currentShakeCoroutine);
+	}
 
-    IEnumerator Shake(Properties props)
-    {
-        float completionPercent = 0;
-        float movePercent = 0;
+	IEnumerator Shake(Properties properties) {
+		float completionPercent = 0;
+		float movePercent = 0;
 
-        float angle_rad = props.angle * Mathf.Deg2Rad - Mathf.PI;
-        Vector3 previousWaypoint = Vector3.zero;
-        Vector3 currentWaypoint = Vector3.zero;
-        float moveDistance = 0;
+		float angle_radians = properties.angle * Mathf.Deg2Rad - Mathf.PI;
+		Vector3 previousWaypoint = Vector3.zero;
+		Vector3 currentWaypoint = Vector3.zero;
+		float moveDistance = 0;
+		float speed = 0;
 
-        while (true)
-        {
-            if (movePercent >= 1 || completionPercent == 0)
-            {
-                float dampingFactor = DampingCurve(completionPercent, props.dampingPercent);
-                float noiseAngle = (Random.value - .5f) * Mathf.PI;
-                angle_rad += Mathf.PI + noiseAngle * props.noisePercent;
-                currentWaypoint = new Vector3(transform.localPosition.x, Mathf.Cos(angle_rad), Mathf.Sin(angle_rad)) * props.strength * dampingFactor;
-                previousWaypoint = transform.localPosition;
+		Quaternion targetRotation = Quaternion.identity;
+		Quaternion previousRotation = Quaternion.identity;
 
-                moveDistance = Vector3.Distance(currentWaypoint, previousWaypoint);
-                movePercent = 0;
-            }
-            completionPercent += Time.deltaTime / props.duration;
-            movePercent += Time.deltaTime / moveDistance * props.speed;
-            transform.localPosition = Vector3.Lerp(previousWaypoint, currentWaypoint, movePercent);
+		do {
+			if (movePercent >= 1 || completionPercent == 0) {
+				float dampingFactor = DampingCurve (completionPercent, properties.dampingPercent);
+				float noiseAngle = (Random.value - .5f) * Mathf.PI;
+				angle_radians += Mathf.PI + noiseAngle * properties.noisePercent;
+				currentWaypoint = new Vector3 (Mathf.Cos (angle_radians), Mathf.Sin (angle_radians)) * properties.strength * dampingFactor;
+				previousWaypoint = transform.localPosition;
+				moveDistance = Vector3.Distance (currentWaypoint, previousWaypoint);
+
+				targetRotation = Quaternion.Euler (new Vector3 (currentWaypoint.y, currentWaypoint.x).normalized * properties.rotationPercent * dampingFactor * maxAngle);
+				previousRotation = transform.localRotation;
+
+				speed = Mathf.Lerp(properties.minSpeed,properties.maxSpeed,dampingFactor);
+
+				movePercent = 0;
+			}
+
+			completionPercent += Time.deltaTime / properties.duration;
+			movePercent += Time.deltaTime / moveDistance * speed;
+			transform.localPosition = Vector3.Lerp (previousWaypoint, currentWaypoint, movePercent);
+			transform.localRotation = Quaternion.Slerp (previousRotation, targetRotation, movePercent);
+	
+
+			yield return null;
+		} while (moveDistance > 0);
+	}
+
+	float DampingCurve(float x, float dampingPercent) {
+		x = Mathf.Clamp01 (x);
+		float a = Mathf.Lerp (2, .25f, dampingPercent);
+		float b = 1 - Mathf.Pow (x, a);
+		return b * b * b;
+	}
 
 
-            yield return null;
-        }
-    }
-    [System.Serializable]
-    public class Properties
-    {
-        public float angle;
-        public float strength;
-        public float speed;
-        public float duration;
-        [Range(0, 1)]
-        public float dampingPercent;
-        [Range(0, 1)]
-        public float noisePercent;
-    }
+	[System.Serializable]
+	public class Properties {
+		public float angle;
+		public float strength;
+		public float maxSpeed;
+		public float minSpeed;
+		public float duration;
+		[Range(0,1)]
+		public float noisePercent;
+		[Range(0,1)]
+		public float dampingPercent;
+		[Range(0,1)]
+		public float rotationPercent;
+
+		public Properties (float angle, float strength, float speed, float duration, float noisePercent, float dampingPercent, float rotationPercent)
+		{
+			this.angle = angle;
+			this.strength = strength;
+			this.maxSpeed = speed;
+			this.duration = duration;
+			this.noisePercent = Mathf.Clamp01(noisePercent);
+			this.dampingPercent = Mathf.Clamp01(dampingPercent);
+			this.rotationPercent = Mathf.Clamp01(rotationPercent);
+		}
+		
+
+	}
 }
